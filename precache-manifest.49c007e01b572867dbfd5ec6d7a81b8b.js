d57b423d8382f62eeedf40ece748ca4f7131ac56
jeanny <jleicaz@gmail.com>
2020-05-04 23:36:39 +0200

    Updates

---
+++ precache-manifest.49c007e01b572867dbfd5ec6d7a81b8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4753bc448e1572ef1329ade30e9f4d5a",
+    "url": "/random-quote/index.html"
+  },
+  {
+    "revision": "3ba13c3f941bcee4366d",
+    "url": "/random-quote/static/css/main.b4178ab0.chunk.css"
+  },
+  {
+    "revision": "574812c83eafa5a4d205",
+    "url": "/random-quote/static/js/2.dc6d6a80.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote/static/js/2.dc6d6a80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ba13c3f941bcee4366d",
+    "url": "/random-quote/static/js/main.d3ad44fb.chunk.js"
+  },
+  {
+    "revision": "7d7485c2a0c643025ae9",
+    "url": "/random-quote/static/js/runtime-main.50c75642.js"
+  }
+]);
