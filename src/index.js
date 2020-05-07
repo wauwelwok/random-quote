55f6243695faacee62dbb647a45c6fe84caeb8da
jeanny <jleicaz@gmail.com>
2020-05-07 20:24:33 +0200

    Added colorchanging background

--- src/index.js
+++ src/index.js
@@ -11,8 +11,9 @@ function TweetButton(props) {
 }
 
 function Quote(props) {
+    const backgroundColor = {backgroundColor: `hsla(${props.colorMod}, 50%, 40%, 0.5)`}
     return (
-        <div className="quote">
+        <div className="quote" style={backgroundColor} >
             <h1 id="text" className="title">"{props.quote.text}"</h1>
             <p id="author" className="subtitle">{props.quote.author}</p>
         </div>
@@ -23,13 +24,15 @@ class QuoteBox extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            quote: {}
+            quote: {},
+            colorMod: 0
         }
+        
         this.handleClick = this.handleClick.bind(this);
     }
 
     componentDidMount() {
-        // Set a random quote when component gets loaded for first time
+        // Set a random quote  with when component gets loaded for first time
         this.fetchQuote();
     }
 
@@ -40,21 +43,22 @@ class QuoteBox extends React.Component {
     fetchQuote() {
         fetch('https://api.quotable.io/random')
         .then((res) => res.json())
-        .then((data) => {
-             
+        .then((data) => {          
             this.setState({
-                quote: {"text": data.content, "author": data.author}
+                quote: {"text": data.content, "author": data.author},
+                // Set modifier for BG color of Quote, based on first letter of author
+                colorMod: 360 / 27 * (data.author.charCodeAt(0) - 64)
             })
         })   
         .catch(console.log)
     }
+
     
     render() {
-        console.log(this.state.quote)
         return (            
             <section className="column is-three-fifths is-offset-one-fifth">
                 <div className="container" id="quote-box">
-                    <Quote quote={this.state.quote} />
+                    <Quote quote={this.state.quote} colorMod={this.state.colorMod}  />
                     <div className="level">
                         <TweetButton quote={this.state.quote} />
                         <button id="new-quote" href="#" onClick={this.handleClick} className="button">New Quote</button>   
